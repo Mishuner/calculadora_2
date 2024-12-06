5d059e62fdcfc8b00404b719a9d35bb50d3d17ad
josepso <pepsort7@gmail.com>
2024-12-06 22:30:01 +0000

    Implementació de l'operació de resta

--- src/main/java/ioc/xtec/appcalculadora/Calculadora.java
+++ src/main/java/ioc/xtec/appcalculadora/Calculadora.java
@@ -16,4 +16,8 @@ public class Calculadora {
    public double sumar(double a, double b) {
       return a + b;
    }
+   
+    public double restar(double a, double b) {
+      return a - b;
+   }
 }
