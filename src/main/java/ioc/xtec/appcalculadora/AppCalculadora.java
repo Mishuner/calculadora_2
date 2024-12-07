d4b58faf1ec2a27dd6742100cc52f5ac44f0bc47
josepso <pepsort7@gmail.com>
2024-12-07 19:51:51 +0000

    Implementació del control d'error en la divisió

--- src/main/java/ioc/xtec/appcalculadora/AppCalculadora.java
+++ src/main/java/ioc/xtec/appcalculadora/AppCalculadora.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  */
 public class AppCalculadora {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException{
         Calculadora c = new Calculadora();
 
       @SuppressWarnings("resource")
